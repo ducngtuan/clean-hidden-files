d74b7a65b8b7612ac4cfce9d2ca7888c1bdf9589
Duc Nguyen <duc.ngtuan@gmail.com>
2017-02-19 11:27:38 +0100

    finished

---
+++ src/com/ducnguyen/Main.java
@@ -0,0 +1,51 @@
+package com.ducnguyen;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Path path;
+        if (args.length == 0) {
+            System.out.print("Enter start path: ");
+            Scanner scanner = new Scanner(System.in);
+            path = Paths.get(scanner.nextLine());
+        } else {
+            path = Paths.get(args[0]);
+        }
+        FileVisitor fv = new FileVisitor();
+        Files.walkFileTree(path, fv);
+        System.out.println("Total scanned " + fv.counted + " items.");
+    }
+
+    public static class FileVisitor extends SimpleFileVisitor<Path> {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/._*");
+        PathMatcher dsStoreMatcher = FileSystems.getDefault().getPathMatcher("glob:**/.DS_Store");
+        PathMatcher thumbDBeMatcher = FileSystems.getDefault().getPathMatcher("glob:**/Thumbs.db");
+        long counted = 0;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if ((matcher.matches(file) &&
+                    attrs.isRegularFile() &&
+                    Files.isHidden(file) &&
+                    Files.exists(file.resolveSibling(file.getFileName().toString().substring(2))))
+                    || dsStoreMatcher.matches(file)
+                    || thumbDBeMatcher.matches(file)) {
+                System.out.println(" -- remove: " + file.toAbsolutePath());
+                Files.delete(file);
+            }
+            counted++;
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            System.out.println("ERROR: " + exc);
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
